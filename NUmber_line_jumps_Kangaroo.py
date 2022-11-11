b1bd9076c2b3b65815a876357e02fc4e23b2274b
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-11-11 12:20:41 +0530

    Update NUmber_line_jumps_Kangaroo.py

--- NUmber_line_jumps_Kangaroo.py
+++ NUmber_line_jumps_Kangaroo.py
@@ -13,3 +13,12 @@ def kangaroo(x1, v1, x2, v2):
             return "YES"
    return "NO
 
+##SOLUTION 2:
+def kangaroo(x1, v1, x2, v2):
+    if v2>v1:
+       return "NO"
+    if (x2-x1) % (v1-v2) == 0:
+        return "YES"
+    else :
+        return "NO"
+
