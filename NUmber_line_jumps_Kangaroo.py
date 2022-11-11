59307a94efd2e4589066109148f9ff898533e76d
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-11-11 11:03:03 +0530

    Number Line Jumps - Kangaroo Jumping

---
+++ NUmber_line_jumps_Kangaroo.py
@@ -0,0 +1,14 @@
+"""
+NUmber LIne JUmps -- Kangaroo
+
+"""
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    if v2> v1 :
+        return "NO"
+    while x1<x2:
+        x1 = x1+v1
+        x2 = x2+v2
+        if x1 == x2 :
+            return "YES"
+
